72cb808d04b67537c41776c696a4c3d76273b7a3
Andrea Nieto Gil <145754377+Tamala24@users.noreply.github.com>
2024-05-22 11:29:06 +0200

    QR with emergency stop

---
+++ ESP32-S3-IoT-QR-ParEm/qr.ino
@@ -0,0 +1,72 @@
+/**
+ * @file  qr.ino
+ * @brief Configuración de la cámara para la lectura de códigos QR
+ */
+
+#include "qr.h"
+
+/**
+ * @brief Configura e inicializa la cámara
+ */
+void config_camara()
+{
+  /* Camera configuration. */
+  Serial.println("Start configuring and initializing the camera...");
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 10000000;
+  config.pixel_format = PIXFORMAT_GRAYSCALE;
+  config.frame_size = FRAMESIZE_QVGA;
+  config.jpeg_quality = 15;
+  config.fb_count = 1;
+  
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    ESP.restart();
+  }
+  
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_QVGA);
+  
+  Serial.println("Configure and initialize the camera successfully.");
+  Serial.println();}
+
+/**
+ * @brief Muestra los resultados de lectura del código QR
+ * 
+ * @param buff. Buffer para guardar el payload del código QR
+ * @param data. Estructura con la información del código QR
+ */
+void dumpData_bis(Buffer_Circ_String_t * buff, const struct quirc_data * data)
+{
+  char QRCodeResult[10];
+  Serial.printf("Version: %d\n", data->version);
+  Serial.printf("ECC level: %c\n", "MLHQ"[data->ecc_level]);
+  Serial.printf("Mask: %d\n", data->mask);
+  Serial.printf("Length: %d\n", data->payload_len);
+  Serial.printf("Payload: %s\n", data->payload);
+  
+  strcpy(QRCodeResult,(const char *)data->payload);
+  Serial.printf("QRCR: %s", QRCodeResult);
+  put_item(QRCodeResult, buff);
+}
+
+/*** End of file ****/
